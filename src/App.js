8b3271f3a90e76b5fc44d029c91312d7d28cc71b
marc-gil <marcgil.code@gmail.com>
2021-03-31 00:43:20 +0200

    Obtain Rank from Api

--- src/App.js
+++ src/App.js
@@ -10,11 +10,15 @@ function App() {
   const [pokemonSpecies, setPokemonSpecies] = useState([]);
   const [selectedSpecie, setSelectedSpecie] = useState(null);
   const [calculatedSpecie, setCalculatedSpecie] = useState(null);
+  const [greatLeague, setGreatLeague] = useState({rank: 0, level: 0, cp: 0});
+  const [ultraLeague, setUltraLeague] = useState({rank: 0, level: 0, cp: 0});
+  const [masterLeague, setMasterLeague] = useState({rank: 0, level: 0, cp: 0});
+
   useEffect(() => {
     fetch('http://localhost:8080/api/pokemon-species')
       .then(response => response.json())
       .then(data => data.map(entry => {
-        return {value: entry.name, label: entry.name, image: entry.image}
+        return {value: entry.name, label: entry.name, image: entry.image, pokedexNumber: entry.pokedex_number}
       }))
       .then(list => setPokemonSpecies(list))
   }, [] );
@@ -22,7 +26,21 @@ function App() {
   function updateCalculatedSpecie() {
     console.log("Selected specie " + selectedSpecie);
     setCalculatedSpecie(selectedSpecie);
+    fetch('http://localhost:8080/api/pokemon-species/' + selectedSpecie.pokedexNumber + '/rank?attackIv=0&cp=1498&defenseIv=15&healthIv=10&levelCap=50')
+      .then(response => response.json())
+      .then(data => {
+        debugger;
+        setGreatLeague(data.greatLeagueRank);
+        setUltraLeague(data.ultraLeagueRank);
+        setMasterLeague(data.masterLeagueRank);
+    })
+      //.then(data => data.map(entry => {
+        //return {value: entry.name, label: entry.name, image: entry.image}
+      //}))
+      //.then(list => setPokemonSpecies(list))
   };
+
+  console.log("great League " + greatLeague);
 
   return (
     <div Style={"max-width: 700px; box-sizing: border-box;margin: 0 auto 20px;"}>
@@ -31,15 +49,9 @@ function App() {
       <Appraisal attack="0" defense="0" health="0"/>
       <SendForm onSubmit={updateCalculatedSpecie}/>
       <Rank image={calculatedSpecie !== null ? calculatedSpecie.image : ""}
-            greatRank="3" 
-            ultraRank="43" 
-            masterRank="278"
-            greatLevel="26.5"
-            ultraLevel="37"
-            masterLevel="50"
-            greatCp="1490"
-            ultraCp="2482"
-            masterCp="3218"/>
+            greatLeague={greatLeague}
+            ultraLeague={ultraLeague} 
+            masterLeague={masterLeague}/>
     </div>
   )
 }
--- src/rank/Rank.js
+++ src/rank/Rank.js
@@ -2,18 +2,22 @@ import Table from "../table/Table";
 import "./Rank.css";
 
 function Rank(props) {
+    const {rank: greatRank, level: greatLevel, cp: greatCp} = props.greatLeague;
+    const {rank: ultraRank, level: ultraLevel, cp: ultraCp} = props.ultraLeague;
+    const {rank: masterRank, level: masterLevel, cp: masterCp} = props.masterLeague;
+    console.log(greatRank);
     return (
         <div className="rank">
             <img alt="" className="picture" src={props.image}></img>
-            <Table greatRank={props.greatRank} 
-                   ultraRank={props.ultraRank} 
-                   masterRank={props.masterRank}
-                   greatLevel={props.greatLevel}
-                   ultraLevel={props.ultraLevel}
-                   masterLevel={props.masterLevel}
-                   greatCp={props.greatCp}
-                   ultraCp={props.ultraCp}
-                   masterCp={props.masterCp}/>
+            <Table greatRank={greatRank} 
+                   ultraRank={ultraRank} 
+                   masterRank={masterRank}
+                   greatLevel={greatLevel}
+                   ultraLevel={ultraLevel}
+                   masterLevel={masterLevel}
+                   greatCp={greatCp}
+                   ultraCp={ultraCp}
+                   masterCp={masterCp}/>
         </div>
     )
 }
